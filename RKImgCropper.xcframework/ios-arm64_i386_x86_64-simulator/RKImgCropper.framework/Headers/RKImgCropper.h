65f18011ec5f8d770cd7b2eb39653a8a8765aa3c
Rezaul <rezaulkarim.imraan@gmail.com>
2021-01-07 15:39:41 +0600

    initial commit

---
+++ RKImgCropper.xcframework/ios-arm64_i386_x86_64-simulator/RKImgCropper.framework/Headers/RKImgCropper.h
@@ -0,0 +1,18 @@
+//
+//  RKImgCropper.h
+//  RKImgCropper
+//
+//  Created by ITDD on 5/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RKImgCropper.
+FOUNDATION_EXPORT double RKImgCropperVersionNumber;
+
+//! Project version string for RKImgCropper.
+FOUNDATION_EXPORT const unsigned char RKImgCropperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKImgCropper/PublicHeader.h>
+
+
